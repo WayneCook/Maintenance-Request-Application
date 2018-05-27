6d8924293bc8e1d675e3d7253fd938fcb49ee40c
Wayne Cook <WCook@superiorgrocers.com>
2018-05-26 22:19:26 -0700

    upload all

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('login/facebook', 'Auth\loginController@redirectToProvider')->name('login.facebook');
-Route::get('login/facebook/callback', 'Auth\loginController@redirectToProvider');
+Route::get('login/facebook/callback', 'Auth\loginController@handleProviderCallback');
 
 Route::get('/home', 'HomeController@index')->name('home');
