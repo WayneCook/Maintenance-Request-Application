ff3de38aa0a095d70834f36f12663c8f73296442
Wayne Cook <WCook@superiorgrocers.com>
2018-05-26 21:30:04 -0700

    upload all

--- app/Http/Controllers/SocialAuthFacebookController.php
+++ app/Http/Controllers/SocialAuthFacebookController.php
@@ -7,52 +7,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Socialite;
 use App\Services\SocialFacebookAccountService;
-use App\User;
 use Auth;
 
 class SocialAuthFacebookController extends Controller
 {
   /**
- * Create a new controller instance.
- *
- * @return void
- */
-public function redirect()
-{
-    return Socialite::driver('facebook')->redirect();
-}
-
-
-/**
- * Create a new controller instance.
- *
- * @return void
- */
-public function callback()
-{
-    try {
-        $user = Socialite::driver('facebook')->user();
-
-        $create['name'] = $user->getName();
-        $create['email'] = $user->getEmail();
-        $create['facebook_id'] = $user->getId();
-        $create['password'] = 'wayne1980';
-
-
-        $userModel = new User;
-        $createdUser = $userModel->addNew($create);
-        Auth::loginUsingId($createdUser->id);
-
-
-        return redirect()->route('home');
-
-
-    } catch (Exception $e) {
-
-
-        return redirect('auth/facebook');
-
-
+   * Create a redirect method to facebook api.
+   *
+   * @return void
+   */
+    public function redirect()
+    {
+        return Socialite::driver('facebook')->redirect();
     }
-  }
+
+    /**
+     * Return a callback method from facebook api.
+     *
+     * @return callback URL from facebook
+     */
+    public function callback(SocialFacebookAccountService $service)
+    {
+
+        return Socialite::driver('google')->stateless()->user();
+
+        auth()->login($user);
+        return redirect()->to('/home');
+    }
 }
