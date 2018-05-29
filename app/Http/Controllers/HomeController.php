1808c7d8c2b808cea150625c54b2f69e8d0c59bf
Wayne Cook <WCook@superiorgrocers.com>
2018-05-28 19:37:16 -0700

    upload all

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,8 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:facebookUser');
+
+        $this->middleware('auth')->except('auth:facebookUser');
     }
 
     /**
