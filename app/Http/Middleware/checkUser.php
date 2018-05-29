d239b5b3a7406a84130bbc2ea6059cdf82e629bb
Wayne Cook <WCook@superiorgrocers.com>
2018-05-28 21:10:42 -0700

    upload all

--- app/Http/Middleware/checkUser.php
+++ app/Http/Middleware/checkUser.php
@@ -18,7 +18,8 @@ class checkUser
 
       if (auth()->check() || auth()->guard('facebookUser')->check())
            {
-             dd(auth()->user());
+             $user = User::guard()->whereEmail($providerUser->getEmail())->first();
+
 
                     return $next($request);
            } else
